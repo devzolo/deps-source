ee751604bb9ba9652b919547c3cf3a7e852ce117
Raphael Zolotorevsky da Motta <zolotorevsky@gmail.com>
2020-02-19 00:57:08 -0300

    Updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # nodejs-typescript-project-template
 Template repository for nodejs typescript projects
+
+https://medium.com/henriquekuwai/criando-sua-cli-com-node-js-d6dee7d03110
+https://www.npmjs.com/package/chalk
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,22 @@
-const str = 'Hello World!';
-console.log(str);
+import program from 'commander';
+import fs from 'fs';
+import path from 'path';
+
+const packageJsonPath = path.resolve(path.join(__dirname, '..', 'package.json'));
+
+const packageJson = JSON.parse(fs.readFileSync(packageJsonPath).toString());
+
+program.version(packageJson.version);
+
+program
+  .command('get')
+  .description('Adiciona um to-do')
+  .action(() => {
+    if (packageJson.deps) {
+      Object.keys(packageJson.deps).forEach(dep => {
+        console.log(dep, ' - ', packageJson.deps[dep].url);
+      });
+    }
+  });
+
+program.parse(process.argv);
